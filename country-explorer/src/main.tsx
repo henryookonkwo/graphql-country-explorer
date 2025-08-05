c651bfa86d9144fa5e4325bf2cb837c8b24b8a58
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2025-08-04 21:19:04 -0400

    Set Up React and Apollo

--- country-explorer/src/main.tsx
+++ country-explorer/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com',
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
